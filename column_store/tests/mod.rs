181d696b35da15d2ea3b8f9632adcfffd51ff666
Erik Nordstrøm <erik@nordstroem.no>
2021-09-17 18:15:46 +0200

    Use derive-new.

--- column_store/tests/mod.rs
+++ column_store/tests/mod.rs
@@ -1,10 +1,11 @@
 use tempfile::tempdir;
+use derive_new::new;
 
 #[macro_use]
 use column_store::get_first_match;
 use column_store::Table;
 
-#[derive(Table)]
+#[derive(Table, new)]
 struct Example1TableRecord {
   a: u64,
   b: u64,
@@ -16,8 +17,8 @@ struct Example1TableRecord {
 fn test_get_first_match_1 () {
   let db_dir = tempdir().unwrap();
   let mut example_table = Example1Table::try_new(db_dir.path()).unwrap();
-  example_table.insert_one(Example1TableRecord { a: 13, b: 37, c: 42, d: "Hello World!".into() });
-  example_table.insert_one(Example1TableRecord { a: 23, b: 23, c: 90, d: "Hot pepper sauce!".into() });
+  example_table.insert_one(Example1TableRecord::new(13, 37, 42, "Hello World!".into()));
+  example_table.insert_one(Example1TableRecord::new(23, 23, 90, "Hot pepper sauce!".into()));
 
   let (a1, d1) = get_first_match!(a, d; example_table; a > 20).unwrap();
   assert_eq!(a1, 23);
@@ -28,8 +29,8 @@ fn test_get_first_match_1 () {
 fn test_get_first_match_2 () {
   let db_dir = tempdir().unwrap();
   let mut example_table = Example1Table::try_new(db_dir.path()).unwrap();
-  example_table.insert_one(Example1TableRecord { a: 13, b: 37, c: 42, d: "Hello World!".into() });
-  example_table.insert_one(Example1TableRecord { a: 23, b: 23, c: 90, d: "Hot pepper sauce!".into() });
+  example_table.insert_one(Example1TableRecord::new(13, 37, 42, "Hello World!".into()));
+  example_table.insert_one(Example1TableRecord::new(23, 23, 90, "Hot pepper sauce!".into()));
 
   let (c2, d2) = get_first_match!(c, d; example_table; b > 20).unwrap();
   assert_eq!(c2, 42);
